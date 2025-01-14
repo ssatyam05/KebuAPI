c20392edc09e254dfc29e7f6a9a45c833447a91d
Satyam Mishra <71537294+ssatyam05@users.noreply.github.com>
2025-01-14 22:13:43 +0530

    First Commit

---
+++ KebuAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KebuAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
